a87eb44704a272a30df4d8092cf53abc0cf0aa84
demariadaniel <ddemaria@brainstation.io>
2018-06-07 15:39:54 -0400

    BUild Version

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+require('dotenv').config()
+
+
+app.use(express.static(__dirname + './../build'))
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log('Server Started');
+	console.log('Press CTRL + C to stop server');
+});
+
+app.get('/env', (req, res)=>{
+  res.send(process.env.API_KEY)
+})
+
+app.get('*', (req, res) => { 
+  res.sendFile('index.html',{root: __dirname + './../build'}) 
+});
