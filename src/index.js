c62d0a9bb58d9db1aa84a9c0a2879837b4899d33
demariadaniel <ddemaria@brainstation.io>
2018-06-13 09:57:53 -0400

    Hot Reloading

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './styles/index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 
+if(module.hot){
+  module.hot.accept()
+}
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
